ad89581766d6718ef02f6d4cf52edc58cf66b234
soogoori <soobin0209@naver.com>
2024-04-26 15:01:07 +0900

    🚀 chore: Dockerfile 생성

---
+++ src/test/java/com/example/whatseoul/WhatSeoulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.whatseoul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatSeoulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
