364dd65eb83c78f1acf827a7357640afdd573574
gyenug <vovogo03@gmail.com>
2024-04-30 02:10:24 +0900

    user 서비스 구현

---
+++ src/main/java/com/example/whatseoul/UserDetailService.java
@@ -0,0 +1,20 @@
+package com.example.whatseoul;
+
+import lombok.RequiredArgsConstructor;
+
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public User loadUserByUsername(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new IllegalArgumentException((email)));
+    }
+}
