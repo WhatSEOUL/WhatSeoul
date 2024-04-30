f6e746fa260ffc801c961032232b523115d0eb5d
gyenug <vovogo03@gmail.com>
2024-04-30 17:06:39 +0900

    ✨feat user 서비스 구현

---
+++ src/main/java/com/example/whatseoul/service/UserService.java
@@ -0,0 +1,21 @@
+package com.example.whatseoul;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public Long save(AddUserRequest dto) {
+        return userRepository.save(User.builder()
+                .email(dto.getEmail())
+                .password(bCryptPasswordEncoder.encode(dto.getPassword()))
+                .build()).getId();
+    }
+}
