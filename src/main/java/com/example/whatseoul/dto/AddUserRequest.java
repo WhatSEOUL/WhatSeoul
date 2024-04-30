0c40474cf2d4292d578736567420a29940576d1d
gyenug <vovogo03@gmail.com>
2024-04-30 17:08:10 +0900

    ✨feat adduser리퀘스트 구현

---
+++ src/main/java/com/example/whatseoul/dto/AddUserRequest.java
@@ -0,0 +1,11 @@
+package com.example.whatseoul.dto;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class AddUserRequest {
+    private String email;
+    private String password;
+}
