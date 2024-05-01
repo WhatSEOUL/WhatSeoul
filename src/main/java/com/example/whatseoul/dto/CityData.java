b4ff51147fb294c93a3ccb1c11adcf49afa6ba91
chc <311mikecho@gmail.com>
2024-05-01 16:35:43 +0900

    ✨ feat: 서울 실시간 도시데이터 - 문화 행사 API 구현

--- src/main/java/com/example/whatseoul/dto/CityData.java
+++ src/main/java/com/example/whatseoul/dto/CityData.java
@@ -1,13 +1,19 @@
 package com.example.whatseoul.dto;
 
+import com.example.whatseoul.entity.CultureEvent;
 import com.example.whatseoul.entity.Population;
+import com.example.whatseoul.entity.PopulationForecast;
 import com.example.whatseoul.entity.Weather;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+
+import java.util.List;
 
 @AllArgsConstructor
 @Getter
 public class CityData {
     private Weather weather;
     private Population population;
+    private CultureEvent cultureEvent;
+    private List<PopulationForecast> pplForecast;
 }
