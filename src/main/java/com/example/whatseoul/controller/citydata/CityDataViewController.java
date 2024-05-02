65964394226f06fe88497a16edcaaced6c0c4555
Hanbit <ghiesf@naver.com>
2024-05-02 13:20:29 +0900

    ✨ feat: 도시데이터(날씨, 인구, 문화 전체) 조회 ui 1차 구현

---
+++ src/main/java/com/example/whatseoul/controller/citydata/CityDataViewController.java
@@ -0,0 +1,42 @@
+package com.example.whatseoul.controller.citydata;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.whatseoul.service.ApiScheduler;
+
+@Controller
+public class CityDataViewController {
+
+	private final ApiScheduler apiScheduler;
+
+	public CityDataViewController(ApiScheduler apiScheduler) {
+		this.apiScheduler = apiScheduler;
+	}
+
+	@GetMapping("/")
+	public String indexPage() {
+		return "/index/index";
+	}
+
+	@GetMapping("/district")
+	public String selectDistrictPage() {
+		return "/selectarea/selectdistrict";
+	}
+
+	@GetMapping("/area")
+	public String selectAreaNamePage() {
+		return "/selectarea/selectareaname";
+	}
+
+	@GetMapping("/area/confirm")
+	public String selectAreaNameConfirmPage() {
+		return "/selectarea/selectconfirm";
+	}
+
+	// 유저가 지역구 선택한 후 "네, 좋아요" 버튼 클릭했을 때 도시데이터를 조회하는 화면
+	@GetMapping("/citydata")
+	public String citydataPage() {
+		return "/citydata/citydatalist";
+	}
+}
--- src/main/resources/static/js/selectarea/selectconfirm.js
+++ src/main/resources/static/js/selectarea/selectconfirm.js
@@ -1,21 +1,3 @@
 const strongText = document.querySelector('.main-content-text strong');
 const areaName = localStorage.getItem('area');
 strongText.innerHTML = areaName;
-
-const confirmButton = document.querySelector('#confirmButton');
-confirmButton.addEventListener('click', function() {
-    fetch('/citydata', {
-        method: 'GET',
-    })
-        .then(response => {
-            console.log("here")
-            console.log(response.text());
-        })
-        .then(xmlText => {
-            // XML 문자열을 XML
-        })
-        .catch(error => {
-            console.error(error);
-        })
-
-})
