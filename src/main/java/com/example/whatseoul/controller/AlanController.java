eca269c3de07bf080564a3d040a05c95fc098935
Hanbit <ghiesf@naver.com>
2024-05-03 00:40:05 +0900

    🐛 fix: 앨런 api - 기본 질의 답변 json 파싱

--- src/main/java/com/example/whatseoul/controller/AlanController.java
+++ src/main/java/com/example/whatseoul/controller/AlanController.java
@@ -6,7 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.whatseoul.dto.response.AlanResponseDto;
 import com.example.whatseoul.service.AlanService;
+import com.fasterxml.jackson.core.JsonProcessingException;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -19,10 +21,9 @@ public class AlanController {
 	private final AlanService alanService;
 
 	@GetMapping("/alan")
-	public ResponseEntity<String> getAlanResponse(@RequestParam String content) {
-		String response = alanService.fetchAlanResponse(content);
-		log.info("response: {}", response);
-
+	public ResponseEntity<AlanResponseDto> getAlanResponse(@RequestParam String content) throws
+		JsonProcessingException {
+		AlanResponseDto response = alanService.fetchAlanResponse(content);
 		return ResponseEntity.ok(response);
 	}
 }
--- src/main/java/com/example/whatseoul/service/AlanService.java
+++ src/main/java/com/example/whatseoul/service/AlanService.java
@@ -1,12 +1,18 @@
 package com.example.whatseoul.service;
 
 
+import org.apache.tomcat.util.json.JSONParser;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import com.example.whatseoul.dto.response.AlanResponseDto;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -18,19 +24,26 @@ public class AlanService {
 	@Value("${alan.api.client_id}")
 	private String CLIENT_ID;
 	private final RestTemplate restTemplate;
+	private final ObjectMapper objectMapper;
 
-	public AlanService(RestTemplateBuilder restTemplateBuilder) {
+	public AlanService(RestTemplateBuilder restTemplateBuilder, ObjectMapper objectMapper) {
 		this.restTemplate = restTemplateBuilder.build();
+		this.objectMapper = objectMapper;
 	}
 
-	public String fetchAlanResponse(String content) {
+	public AlanResponseDto fetchAlanResponse(String content) throws JsonProcessingException {
 		String uri = UriComponentsBuilder
 			.fromHttpUrl(BASE_URL)
 			.queryParam("content", content)
 			.queryParam("client_id", CLIENT_ID)
 			.toUriString();
 
-		log.info("uri: {}", uri);
-		return restTemplate.getForObject(uri, String.class);
+		ResponseEntity<String> response =  restTemplate.getForEntity(uri, String.class);
+		String responseBody = response.getBody();
+		return parseJsonResponse(responseBody);
+	}
+
+	private AlanResponseDto parseJsonResponse(String responseBody) throws JsonProcessingException {
+		return objectMapper.readValue(responseBody, AlanResponseDto.class);
 	}
 }
