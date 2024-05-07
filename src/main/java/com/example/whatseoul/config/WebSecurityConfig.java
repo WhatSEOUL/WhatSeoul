fa4de7370a7f213d2c92a41868911f1bccf9ba49
gyenug <vovogo03@gmail.com>
2024-05-07 09:26:04 +0900

    ✨feat WebSecurityConfigconfig 구현

--- src/main/java/com/example/whatseoul/config/WebSecurityConfig.java
+++ src/main/java/com/example/whatseoul/config/WebSecurityConfig.java
@@ -1,8 +1,10 @@
-package com.example.whatseoul;
+
+
+package com.example.whatseoul.config;
 
 import lombok.RequiredArgsConstructor;
 
-
+import com.example.whatseoul.service.UserDetailService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
