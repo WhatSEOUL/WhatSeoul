a8a0c6e4a153da03a7e7d75c97097a8915f5ca15
gyenug <vovogo03@gmail.com>
2024-05-07 09:24:59 +0900

    ✨feat user레포지토리 구현

--- src/main/java/com/example/whatseoul/repository/UserRepository.java
+++ src/main/java/com/example/whatseoul/repository/UserRepository.java
@@ -1,7 +1,9 @@
-package com.example.whatseoul;
+package com.example.whatseoul.repository;
 
+
+//com.example.whatseoul.
+import com.example.whatseoul.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
