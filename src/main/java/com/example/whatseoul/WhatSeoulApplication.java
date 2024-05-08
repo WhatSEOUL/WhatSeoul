73b45b4b45547184a64dfbb52ad0664e4f998b90
gyenug <vovogo03@gmail.com>
2024-05-08 13:05:43 +0900

    ✨feat WhatSeoulApplication.java jpa 추가

--- src/main/java/com/example/whatseoul/WhatSeoulApplication.java
+++ src/main/java/com/example/whatseoul/WhatSeoulApplication.java
@@ -3,7 +3,12 @@ package com.example.whatseoul;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//추가
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
 @SpringBootApplication
+@EnableJpaAuditing
+
 public class WhatSeoulApplication {
 
     public static void main(String[] args) {
