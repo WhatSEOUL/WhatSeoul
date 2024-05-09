ffd082da5a9c2f2bf98b2490f46579364b58855e
Hanbit <ghiesf@naver.com>
2024-05-09 23:20:15 +0900

    🐛 fix : 유저 관련 api url 매핑 정리

--- src/main/java/com/example/whatseoul/controller/account/AccountController.java
+++ src/main/java/com/example/whatseoul/controller/account/AccountController.java
@@ -16,24 +16,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("/")
 @RequiredArgsConstructor
 public class AccountController {
 
     private final AccountService accountService;
 
-    @GetMapping("login")
+    @GetMapping("/login")
     public String login() {
 
         return "login";  // 로그인 페이지를 반환
     }
 
-    @GetMapping("register")
+    @GetMapping("/register")
     public String register() {
         return "register";  // 회원가입 페이지를 반환
     }
 
-    @PostMapping("join")
+    @PostMapping("/api/join")
     public String join(String email, String password, String name) {
         // 회원가입 로직 처리
         // 성공하면 로그인 페이지로 리디렉트
@@ -42,14 +41,14 @@ public class AccountController {
         return "redirect:/login";
     }
 
-    @GetMapping("logout")
+    @GetMapping("/api/logout")
     public String logout(HttpServletRequest request, HttpServletResponse response) {
         new SecurityContextLogoutHandler().logout(request, response,
             SecurityContextHolder.getContext().getAuthentication());
         return "redirect:/";
     }
 
-    @GetMapping("check/username")
+    @GetMapping("/api/check/username")
     public ResponseEntity<String> checkDuplicateUserName(@RequestParam("name") String userName) {
         if (accountService.existsByUserName(userName)) {
             return ResponseEntity.badRequest().body("이미 사용중인 유저네임입니다.");
@@ -58,7 +57,7 @@ public class AccountController {
         }
     }
 
-    @GetMapping("check/email")
+    @GetMapping("/api/check/email")
     public ResponseEntity<String> checkDuplicatUserEmail(@RequestParam("email") String userEmail) {
         if (accountService.existsByUserEmail(userEmail)) {
             return ResponseEntity.badRequest().body("이미 사용중인 이메일입니다.");
