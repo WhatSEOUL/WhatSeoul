8da205a986d9e2cdc6635cdde36721a9e11b6689
gyenug <vovogo03@gmail.com>
2024-05-09 15:26:06 +0900

    ✨feat 0509 한번에 develop머지

---
+++ src/main/java/com/example/whatseoul/entity/User.java
@@ -0,0 +1,34 @@
+package com.example.whatseoul.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+
+import javax.validation.constraints.Email;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+
+    @Column(name = "USER_EMAIL", nullable = false, length = 255) // length(ERD 포함)  길이 255로 변경했습니다.
+    private String userEmail;
+
+    @Column(name = "USER_PASSWORD", nullable = false, length = 100) //length(ERD 포함)  길이 100으로 변경했습니다.
+    private String userPassword;
+
+    @Column(name = "USER_NAME", nullable = false, length = 30) //length(ERD 포함)  길이 30으로 변경했습니다.
+    private String userName;
+
+    @Column(name = "USER_CREATED")
+    @CreatedDate
+    private LocalDateTime userCreated;
+}
