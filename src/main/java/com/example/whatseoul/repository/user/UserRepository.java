02352e787bb4bab55e3249f74f96156ac508bba5
gyenug <vovogo03@gmail.com>
2024-05-13 03:08:24 +0900

    ✨feat UserRepository 변경

--- src/main/java/com/example/whatseoul/repository/user/UserRepository.java
+++ src/main/java/com/example/whatseoul/repository/user/UserRepository.java
@@ -6,10 +6,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
+
+
+    boolean existsByUserName(String userName);
+    boolean existsByUserEmail(String userEmail);
+
+
     Optional<User> findById(Long id);
 
     Optional<User> findByUserName(String userName);
     Optional<User> findByUserEmail(String email); // 이메일을 기준으로 사용자 찾기
-    boolean existsByUserName(String userName);
-    boolean existsByUserEmail(String userEmail);
+
 }
