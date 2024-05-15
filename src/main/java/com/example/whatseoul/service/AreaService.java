2235a137efd8512b8eab8c0e228052c57b232f6b
Hanbit <ghiesf@naver.com>
2024-05-15 11:52:28 +0900

    🔥 Remove : 카카오맵 api key 분리 원상복구

--- src/main/java/com/example/whatseoul/service/AreaService.java
+++ src/main/java/com/example/whatseoul/service/AreaService.java
@@ -10,7 +10,6 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
@@ -138,10 +137,4 @@ public class AreaService {
 			.y(y)
 			.build();
 	}
-
-	public String getKakaoKey() {
-		return KAKAO_KEY;
-	}
-
-
 }
