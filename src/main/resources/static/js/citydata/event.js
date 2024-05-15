aec3bbae8ec53c8bcde1dadf6e38d33771467b29
Hanbit <ghiesf@naver.com>
2024-05-15 13:46:00 +0900

    🐛 fix : 문화행사 데이터 조회 css 수정

--- src/main/resources/static/js/citydata/event.js
+++ src/main/resources/static/js/citydata/event.js
@@ -69,6 +69,7 @@ function showEventData(event) {
     const eventUrl = document.createElement('a');
     eventUrl.textContent = `${event.culturalEventUrl}`;
     eventUrl.classList.add('card__describe'); // 클래스 추가
+    eventUrl.id = "card__eventUrl";
     eventUrl.href = event.culturalEventUrl;
     eventUrl.target = "_blank";
     eventContent.appendChild(eventUrl);
